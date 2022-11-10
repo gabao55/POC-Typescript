61f83435bc3071d27f751e7a3922aeb81481cc43
gabao55 <gabriel.s.rosin@gmail.com>
2022-11-10 18:46:43 -0300

    Adds cors to the API

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import taskRouter from './routers/task.router.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 const server = express();
-server.use(express.json());
-
-server.use(taskRouter);
+server
+    .use(express.json())
+    .use(taskRouter)
+    .use(cors());
 
 server.listen(process.env.PORT, void console.log(`Listening to PORT ${process.env.PORT}`));
