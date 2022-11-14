01ba4c5179fd5a49aab976eda96bba47aa7c641a
Gabriel Rosin <GABRIEL.S.ROSIN@GMAIL.COM>
2022-11-14 15:54:35 -0300

    Delete dist directory

--- dist/schemas/task.schema.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-var TaskSchema = Joi.object({
-    name: Joi.string().min(1).required(),
-    description: Joi.string().min(1).required(),
-    deadline: Joi.date().required()
-});
-export { TaskSchema };
--- dist/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import taskRouter from './routers/task.router.js';
-import dotenv from 'dotenv';
-dotenv.config();
-var server = express();
-server.use(express.json());
-server.use(taskRouter);
-server.listen(process.env.PORT, void console.log("Listening to PORT ".concat(process.env.PORT)));
